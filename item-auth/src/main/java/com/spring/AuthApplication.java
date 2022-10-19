d74f078027c214d456231298c09edef428ea95e9
donghaoxie <xie5721072@163.com>
2022-10-19 15:52:36 +0800

    修改代码

--- item-auth/src/main/java/com/spring/AuthApplication.java
+++ item-auth/src/main/java/com/spring/AuthApplication.java
@@ -8,6 +8,5 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class AuthApplication {
     public static void main(String[] args){
         SpringApplication.run(AuthApplication.class, args);
-        System.out.println("auth的application");
     }
 }
