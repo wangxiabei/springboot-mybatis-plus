131ed934e133c77bb9fa8f5b6f76b1d6a7b6bb3f
wangbeicheng <xie5721072@163.com>
2022-10-20 10:04:02 +0800

    修改代码

--- item-auth/src/main/java/com/spring/AuthApplication.java
+++ item-auth/src/main/java/com/spring/AuthApplication.java
@@ -8,5 +8,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class AuthApplication {
     public static void main(String[] args){
         SpringApplication.run(AuthApplication.class, args);
+        System.out.println("1qq");
     }
 }
